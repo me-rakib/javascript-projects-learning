fa22284a3b3969a82e1faef64be40c7f47cd32b8
Rakib Hasan <rakib.cs132@gmail.com>
2021-10-15 01:11:05 +0600

    e com

--- ecommerce-product-page-main/assets/js/main.js
+++ ecommerce-product-page-main/assets/js/main.js
@@ -1,6 +1,14 @@
 const navToggle = document.getElementById("nav-toggle");
 const navClose = document.getElementById("nav-close");
 const navMenu = document.getElementById("nav-menu");
+const cartIcon = document.getElementById("cart-icon");
+const cart = document.getElementById("cart");
+const cartMinus = document.getElementById("cart-minus");
+const cartPlus = document.getElementById("cart-plus");
+const cartCurrentVal = document.getElementById("cart-val");
+const addToCart = document.getElementById("add-to-cart");
+const cartTotal = document.getElementById("cart-total");
+let cartVal = 0;
 
 if (navToggle) {
   navToggle.addEventListener("click", () => {
@@ -21,3 +29,23 @@ function linkAction() {
   navMenu.classList.remove("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+cartIcon.addEventListener("click", () => {
+  cart.classList.toggle("show-cart");
+});
+
+function decreaseHandle() {
+  cartVal--;
+  cartCurrentVal.innerText = cartVal;
+}
+function increaseHandle() {
+  cartVal++;
+  cartCurrentVal.innerText = cartVal;
+}
+
+cartMinus.addEventListener("click", decreaseHandle);
+cartPlus.addEventListener("click", increaseHandle);
+
+addToCart.addEventListener("click", () => {
+  cartTotal.innerText = cartVal;
+});
